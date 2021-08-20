0c4b11146c8a1a8f33f04d593516a564a5a43c1c
Matt <stanfiem@uci.edu>
2021-08-19 22:56:54 -0700

    updating

--- setup_db.sh
+++ setup_db.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+
+sudo apt update
+sudo pat install postgresql postgresql-contrib
+
 sudo -u postgres createuser discordbot
 sudo -u postgres createdb server_settings
-#sudo -u postgres psql 'alter user discordbot with encrypted password <password>;'
-#sudo -u postgres psql 'grant all privileges on database server_settings to discordbot;'
+sudo -u postgres psql -c "ALTER USER discordbot PASSWORD 'discordbotpassword';"
+sudo -u postgres psql -c "grant all privileges on database server_settings to discordbot;"
