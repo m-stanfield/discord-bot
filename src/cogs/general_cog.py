69bd66a4b0f8cfad8551ac5503816e2a1c9b7012
m-stanfield <mstanfield52@gmail.com>
2022-03-02 20:53:53 -0500

    added cogs to src, will later rewrite but just so I can start testing the other rewrites

---
+++ src/cogs/general_cog.py
@@ -0,0 +1,64 @@
+from discord.ext import commands
+import os
+import sys
+import inspirobot
+import functions.profile_fun as pf
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+class general(commands.Cog):
+    def __init__(self, bot):
+        logger.info('Cog Loaded: general')
+        self.bot = bot
+
+    @commands.command()
+    async def inspire(self, ctx, *args):
+        await ctx.send(inspirobot.generate().url)
+        try:
+            await ctx.message.delete()
+        except discord.errors.Forbidden:
+            logger.info("Message could not be deleted: Forbidden Error")
+
+    @commands.command()
+    async def roll(self,ctx,*args):
+        '''Rolls dice. !roll <dice string>. Dice string exmaple: 1d20 -4d2 + 8 -2d2'''
+        await ctx.send(pf.roll_dice(args))
+
+
+    @commands.command()
+    async def nicknames(self,ctx,playerID=None,length=5):
+        '''Displays users past nicknames on server. !nicknames @<mention> <length>. @<mention> only works for superusers'''
+        '''
+        Inputs
+        ------
+        playerID
+            Member ID from @mention in a chat. Optoinal value.
+
+        length (int)
+            length of list generated.
+
+        '''
+        await ctx.message.delete()
+
+        directory='logs/nicknames/'
+        users = self.bot.get_cog('users')
+        #checking if user is superuser and can change other users settings
+        member = await users.find_supermember(ctx,playerID)
+
+        #building message strings from file
+        message = '~~~~~\n'
+        message += 'Nicknames for '+member.name + ' on Server ' + ctx.guild.name + '\n'
+        fileName = directory+ member.name.replace(' ','_') +'_nicknames_'+ctx.guild.name.replace(' ','_') + '.dat'
+        if os.path.exists(fileName):
+            with open(fileName,'r') as f:
+                lines = f.readlines()
+                if len(lines) < length:
+                    length = len(lines)
+
+                for ii in range(length):
+                    splitline = lines[-(ii+1)].replace('\n','').split('\t')
+
+                    message += splitline[0] + '\t' + splitline[2] +'\n'
+            await ctx.send(message)
---
+++ src/cogs/image_cog.py
@@ -0,0 +1,38 @@
+from discord.ext import commands
+import os
+import discord
+import sys
+
+import functions.profile_fun as pf
+import logging
+
+logger = logging.getLogger(__name__)
+
+class images(commands.Cog):
+    def __init__(self, bot):
+        logger.info('Cog Loaded: images')
+        self.bot = bot
+
+    @commands.command()
+    async def upload_image(self,ctx):
+        users = self.bot.get_cog('users')
+        superuser = await users.check_super(ctx.guild, ctx.author)
+        if superuser:
+            if ctx.message.attachments:
+                logger.info(ctx.message.attachments[0].filename)
+                image_extensions = ['.png','.jpg','.gif']
+                if ctx.message.attachments[0].filename[-4:] in image_extensions:
+                    await ctx.message.attachments[0].save('data/images/' + ctx.message.attachments[0].filename)
+
+    async def post_image(self,ctx,fileName):
+        if os.path.isfile(fileName):
+            #logger.info(ctx.message.content)
+            try:
+                await ctx.message.delete()
+            except discord.errors.Forbidden:
+                logger.info('Message could not be deleted:Forbidden Error')
+            #logger.info(ctx.message.content)
+            await ctx.send(f'From: {ctx.author.display_name}',file=discord.File(fileName)) 
+
+
+
