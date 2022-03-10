37868bb43a623246afad788f3e917339c612d8fd
m-stanfield <mstanfield52@gmail.com>
2022-03-09 21:42:46 -0500

    added SQL database and database unit tests

--- src/database/DataBase.py
+++ src/database/DataBase.py
@@ -1,19 +1,21 @@
 from src.logging.logger import Logger
-from src.misc.Settings import Settings
+from src.common.Settings import Settings
 import asyncio
 from sqlalchemy import text
 from sqlalchemy.engine import Engine
 from sqlalchemy.ext.asyncio import create_async_engine
+from typing import Any
 logger = Logger(__name__)
 Settings.init()
 
 class DataBase:
     #DEFAULT_KWARGS = {'path':':memory:'}#
-    DEFAULT_KWARGS = {'path': 'data/database/discord_bot.db','schema_path':'settings/database/schema'}
+    DEFAULT_KWARGS = {'path': 'data/database/discord_bot.db'}
     db:Engine|None = None
 
     def __init__(self, **kwargs):
         self._kwargs = {**self.DEFAULT_KWARGS, **kwargs}
+        self._schema:dict[list[str]] = {}
         if self.db is None:
             self.db = create_async_engine("sqlite+aiosqlite:///"+self._kwargs['path'])
             logger.info(f"Data has been loaded from : {self._kwargs['path']}")
@@ -23,29 +25,77 @@ class DataBase:
         await self.db.dispose()
         logger.info(f"Database was successfully closed")
 
+    async def delete(self, tableName:str):
+        if tableName in self._schema:
+            self._schema.pop(tableName)
+            cmdstr = f"DELETE FROM {tableName}"
+            await self.execute(cmdstr)
+            result = True
+        else:
+            result = False        
+        return result
+    
+    async def _deleteTables(self, tableList:list[str]):
+        for table in tableList:
+            await self.delete(table)
+
+
+    async def _deleteAllTables(self):
+        keys = await self.getTableNames()
+        await self._deleteTables(keys)
+        self._schema = {}
+
+
+    async def _clearAllTables(self):
+        keys = await self.getTableNames()
+
+
+
+    async def getTableNames(self) -> list[str]:
+        cmdstr = "SELECT name FROM sqlite_master WHERE type ='table' AND name NOT LIKE 'sqlite_%';"
+        exe_output = await self.execute(command=cmdstr)
+        results = []
+        for exe_tuple in exe_output.all():
+            results.append(exe_tuple[0])
+        return results
+
 
     async def execute(self, command, args=None):
         logger.info(f"SQL Command: {command}")
-        print(command)
         async with self.db.begin() as conn:
             result = await conn.execute(text(command))
-
-         #   if result.rowcount < 1:
             return result
-           # else:
-           #     return result.all()
+
  
-    async def select(self, cmdstr):
+    async def select(self, tableName:str, values_where:dict[Any]|None=None,columns:list[str]|None=None)-> list[Any]:
+        cmdstr = self._buildSelectString(columns=columns, tableName=tableName, values_where=values_where)
         result = await self.execute(cmdstr)
         result = result.all()
-
-        logger.info(result)
-        if len(result) > 0:
-            result = result[0]
-        logger.info(result)
-        if len(result) == 1:
-            result = result[0]
         return result
+
+    def _buildSelectString(self, tableName:str, values_where:dict[Any]|None=None,columns:list[str]|None=None) -> str:
+        cmdstr = f"SELECT "
+        if columns is None:
+            cmdstr += "*"
+        else:
+            for i, column in enumerate(columns):
+                if i > 0:
+                    cmdstr += ", "
+                cmdstr += column
+            
+            
+        cmdstr += f" FROM {tableName}"
+        if values_where is not None:
+            cmdstr += " WHERE "
+            for idx, (column, value) in enumerate(values_where.items()):
+                if not(idx == 0):
+                    cmdstr += " AND "
+                cmdstr += f"{column} = "
+                if type(value) is str:
+                    cmdstr += f"'{value}'"
+                else:
+                    cmdstr += f"{value}"
+        return cmdstr
 
     async def createTable(self, tableName, columns, column_types=None, column_defaults=None):
         # schema_list is tuple/list of tuples/lists of keyword/datatype pairs
@@ -60,57 +110,61 @@ class DataBase:
             logger.error(errormsg)
             raise ValueError(errormsg)
 
-        # generate command string for creating a table
-        cmdstr = f"CREATE TABLE if not exists {tableName} ("
-        for i in range(len(columns)):
-            # for first element, skip the column
-            if i > 0:
-                cmdstr += ", "
+        current_table = await self.getTableNames()
+        if tableName not in current_table:
 
-            # write ? to position the column name
-            cmdstr += f"{columns[i]}"
+            # generate command string for creating a table
+            cmdstr = f"CREATE TABLE if not exists {tableName} ("
+            for i in range(len(columns)):
+                # for first element, skip the column
+                if i > 0:
+                    cmdstr += ", "
 
-            # if column datatype is given, add ? for its position
-            if column_types is not None:
-                cmdstr += f" {column_types[i]}"
+                # write ? to position the column name
+                cmdstr += f"{columns[i]}"
 
-            if column_defaults is not None:
-                cmdstr += f" DEFAULT {column_defaults[i]}"
-        cmdstr += ")"
+                # if column datatype is given, add ? for its position
+                if column_types is not None:
+                    cmdstr += f" {column_types[i]}"
 
-        await self.execute(cmdstr)
+                if column_defaults is not None:
+                    cmdstr += f" DEFAULT {column_defaults[i]}"
+            cmdstr += ")"
+
+            result = await self.execute(cmdstr)
+            print(result.all())
+        table_columns = await self.getColumnNames(tableName=tableName)
+        self._schema[tableName] = list(table_columns)
         return True
 
-    async def insert(self, table: str, columns: list, values: list) -> bool:
-        if not(len(columns) == len(values)):
-            return False
+    def _buildInsertString(self, table:str, values:dict[str]) -> str:
+        cmdstr = f"INSERT INTO {table} ("
 
-        cmdstr = f"INSERT INTO {table}("
-
-        for idx, elem in enumerate(columns):
+        for idx, elem in enumerate(values.keys()):
             if not(idx == 0):
                 cmdstr += ", "
             cmdstr += elem
         cmdstr += ") VALUES ("
 
-        for idx, elem in enumerate(values):
+        for idx, elem in enumerate(values.values()):
             if idx > 0:
                 cmdstr += ", "
             if type(elem) is str:
-                cmdstr += f'"{elem}"'
+                cmdstr += f"'{elem}'"
             else:
                 cmdstr += str(elem)
         cmdstr += ")"
+        return cmdstr
 
-        await self.execute(cmdstr)
+    async def insert(self, table: str, values:dict[str]) -> bool:
+        if not(set(values.keys()).issubset(set(self._schema[table]))):
+            return False
+        cmdstr = self._buildInsertString(table=table, values=values)
+        return await self.execute(cmdstr)
 
 
-        return True
-
-
-    async def init(self):
-        db_settings = Settings.get('DATA_BASE')
-        print(db_settings)
+    async def init(self, settings:dict[str,tuple[str,Any]]|None = None):
+        db_settings = settings if settings is not None else Settings.get('DATA_BASE')
         for key in db_settings:
             columns = []
             column_types = []
@@ -119,19 +173,30 @@ class DataBase:
                 columns.append(elemName)
                 column_types.append(elemType)
                 column_defaults.append(elemDefault)
-                print(key,elemName, elemDefault,elemType)
-            print(key)
             await self.createTable(key.lower(), columns, column_types=column_types, column_defaults=column_defaults)
 
+    async def getColumnNames(self,tableName):
+        result = await self.execute(f"SELECT * FROM {tableName}")
+        print('columns',result.keys())
+       # col = await self.select(tableName="all_tables",columns=['table_name'])
+       # print(col)
+        return result.keys()
 
 
 if __name__ == "__main__":
     Settings()
     async def main():
-        db = DataBase()
-        await db.init()
-        await db.insert('users',['id'],[1])
-        result = await db.select('users',['id'])
+        TEST_SCHEMA = {'table_one':{"test_INTEGER":('INTEGER',1),"test_TEXT":("TEXT",'abcd'),"test_REAL":("REAL",3.14159),"test_BLOB":("BLOB",1.28187138)}, 
+                            'table_two':{"test_INTEGER":('INTEGER',10),"test_TEXT":("TEXT",'dcba'),"test_REAL":("REAL",1.183),"test_BLOB":("BLOB","TEXTBLOB")}}
+        db = DataBase(path='src/tests/database/test.db')
+        await db.init(settings=TEST_SCHEMA)
+        for i in range(10):
+            await db.insert('table_one',{"test_INTEGER":100})
+        await db._deleteAllTables()
+        #await db.init(settings=TEST_SCHEMA)
+
+
+        #await db.insert('table_one',{"test_INTEGER":100})
+
         await db.close()
-        print(result)
     asyncio.run(main())
---
+++ src/tests/database/DataBase_tests.py
@@ -0,0 +1,76 @@
+import unittest
+import asyncio
+
+from src.database.DataBase import DataBase
+
+class DataBase_tests(unittest.IsolatedAsyncioTestCase):
+    TEST_DATABASE = 'src/tests/database/test.db'
+    TEST_SCHEMA = {'table_one':{"test_INTEGER":('INTEGER',1),"test_TEXT":("TEXT",'abcd'),"test_REAL":("REAL",3.14159),"test_BLOB":("BLOB",1.28187138)}, 
+                     'table_two':{"test_INTEGER":('INTEGER',10),"test_TEXT":("TEXT",'dcba'),"test_REAL":("REAL",1.183),"test_BLOB":("BLOB","TEXTBLOB")}}
+    async def test_Init(self):
+        db = DataBase(path=self.TEST_DATABASE)
+        await db.init(settings=self.TEST_SCHEMA)
+        await db._deleteAllTables()
+
+        for table in self.TEST_SCHEMA.keys():
+            result = await db.getColumnNames(table)
+            assert set(result) == set(self.TEST_SCHEMA[table])
+                
+    async def test_InvalidColumnInsert(self):
+        db = DataBase(path=self.TEST_DATABASE)
+        await db._deleteAllTables()
+        await db.init(settings=self.TEST_SCHEMA)
+
+        insert1 = {'aaaatest_INTEGER':10831, 'test_TEXT':"test_text"}
+        result =  await db.insert('table_one',insert1)
+        assert result == False
+
+    async def test_ValidColumnInsert(self):
+        db = DataBase(path=self.TEST_DATABASE)
+        await db._deleteAllTables()
+        await db.init(settings=self.TEST_SCHEMA)
+
+        insert1 = {'test_INTEGER':10831, 'test_TEXT':"test_text"}
+        selection = await db.select(tableName='table_one',values_where=insert1)
+        print('selection before: ',selection)
+        result =  await db.insert('table_one',insert1)
+
+        selection = await db.select(tableName='table_one',values_where=insert1)
+        print('selection after: ',selection)
+
+        assert result != False
+
+    async def test_buildSelectString(self):
+        
+        db = DataBase(path=self.TEST_DATABASE)
+        await db._deleteAllTables()
+        await db.init(settings=self.TEST_SCHEMA)
+
+        cmdstr = db._buildSelectString('table_one',{'test_INTEGER':1,'test_TEXT':'abcd'})
+        target_cmd = "SELECT * FROM table_one WHERE test_INTEGER = 1 AND test_TEXT = 'abcd'"
+        assert cmdstr == target_cmd
+
+        cmdstr = db._buildSelectString('table_one',{'test_INTEGER':1,'test_TEXT':'abcd'},columns=['test_INTEGER'])
+        target_cmd = "SELECT test_INTEGER FROM table_one WHERE test_INTEGER = 1 AND test_TEXT = 'abcd'"
+        assert cmdstr == target_cmd
+
+    async def test_buildInsertString(self):
+        
+        db = DataBase(path=self.TEST_DATABASE)
+        await db._deleteAllTables()
+        await db.init(settings=self.TEST_SCHEMA)
+
+        cmdstr = db._buildInsertString('table_one',{'test_INTEGER':1,'test_TEXT':'abcd'})
+        target_cmd = "INSERT INTO table_one (test_INTEGER, test_TEXT) VALUES (1, 'abcd')"
+        assert cmdstr == target_cmd
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+    async def main():
+        db = DataBase(path=DataBase_tests.TEST_DATABASE)
+        await db.init()
+        await db._deleteAllTables()
+        await db.close()
+    asyncio.run(main())
