48b16d4fa703838b88d3a3ceb58f349b7a7a1a52
m-stanfield <mstanfield52@gmail.com>
2022-03-11 00:19:51 -0500

    set up the basics of the discord bot to allow for command reloading and an OwerCommands cog

--- src/DiscordBot.py
+++ src/DiscordBot.py
@@ -1,16 +1,25 @@
 import discord
 import asyncio
+
+from discord.ext.commands import Bot
+import time
+
+from importlib import reload
+from src.logging.logger import Logger
 from src.database.BaseDataBase import BaseDataBase
 from src.database.DiscordDataBase import DiscordDataBase
 from src.common.Settings import Settings
 
-from src.logging.logger import Logger
+
+import src.cogs.OwnerCommands as OwnerCommands
+
+cogList = [OwnerCommands]
+
 
 
 logger = Logger(__name__)
-
 Settings.init()
-class DiscordBot(discord.Bot):
+class DiscordBot(Bot):
 
     DEFAULT_KWARGS = {'description':"default description","command_prefix":"!","intents":None}
     def __init__(self, **kwargs):
@@ -25,13 +34,17 @@ class DiscordBot(discord.Bot):
         )
 
         db:BaseDataBase = None if "db" not in kwargs else kwargs.pop('db')
-        print(db)
         if not(isinstance(db,BaseDataBase)):
             path = Settings.get("SQLITE_DB") if "SQLITE_DB" not in kwargs else kwargs.pop("SQLITE_DB")
             db = DiscordDataBase(path=path)
         self.db = db
+        self.add_cog(OwnerCommands.OwnerCommands(self))
 
+        self.run_status = True
 
+    async def close(self):
+        await self.db.close()
+        await super().close()
     
     async def run(self):
         await self.db.init()
@@ -43,16 +56,35 @@ class DiscordBot(discord.Bot):
             logger.info("KeyboardInterrupt: Exiting Program")
             await self.db.close()
             await self.close()
-            
+
+    async def setRunStatus(self, status:bool):
+        self.run_status = status
+
+    def getRunStatus(self):
+        return self.run_status
+
+
+
+
+        
+def reload_modules():
+    for module in cogList:
+        reload(module)
 
 
 
 if __name__ == "__main__":
     async def main():
         print(Settings.get('SQLITE_DB'))
-        bot = DiscordBot(SQLITE_DB=Settings.get('SQLITE_DB'))
-
-        await bot.run()
-    while True:
-        asyncio.run(main())
+        run_status = True
+        while run_status:
+            reload_modules()
+            print(run_status)
+            bot = DiscordBot(SQLITE_DB=Settings.get('SQLITE_DB'))
+            await bot.run()
+            run_status = bot.getRunStatus()
+            time.sleep(10)
+            print('bot quit')
+            print(run_status)
+    asyncio.run(main())
     
---
+++ src/cogs/OwnerCommands.py
@@ -0,0 +1,44 @@
+from discord.client import Client
+import discord
+import asyncio
+from src.database.BaseDataBase import BaseDataBase
+from src.database.DiscordDataBase import DiscordDataBase
+from src.common.Settings import Settings
+from discord.ext import commands
+from discord.ext.commands import Bot
+import time
+from discord.commands import slash_command
+
+from src.logging.logger import Logger
+
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from src.DiscordBot import DiscordBot
+
+
+class OwnerCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot:DiscordBot = bot
+
+    @commands.command()
+    @commands.is_owner()
+    async def shutdown(self,ctx):
+        await self.bot.setRunStatus(False)
+        await self.bot.close()
+
+    @commands.command()
+    @commands.is_owner()
+    async def example(self,ctx,*args):
+        '''Text-to-speech of whatever is said after !say command'''
+        print('author',type(ctx.author))
+        print(ctx.author)
+        print('guild',type(ctx.guild))
+        print(ctx.guild)
+        print('ctx',type(ctx))
+
+    @commands.command()
+    @commands.is_owner()
+    async def restart(self, ctx, *args):
+        await self.bot.close()
+
