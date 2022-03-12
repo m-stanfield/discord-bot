eb0d2ae33f1ec8cc952ec966baaa9d10748625d4
m-stanfield <mstanfield52@gmail.com>
2022-03-12 12:32:34 -0500

    rewrote main to allow for reload of all modules within the project on a !restart call

--- main.py
+++ main.py
@@ -1,38 +1,64 @@
-from src.DiscordBot import DiscordBot
+#external imports
 import asyncio
-import logging
-from src.logging.logger import Logger
-import argparse
-import os
-from src.misc.Settings import Settings
-# globals
-logger = Logger(__name__)
-Settings.init()
+import time
+import importlib 
+import sys
 
-async def main():
+PRELOADED_MODULES = set(sys.modules.values())
+
+
+# Imports for code in this project
+import src.DiscordBot as DiscordBot
+import src.logging.logger as Logger
+import src.common.Settings as Settings
+
+logger = None
+
+def startup_message():
     startup_message = "Discord Bot Startup Began"
     buffer_size = 30
     startup_message = "~"*buffer_size + startup_message + "~"*buffer_size
     logger.info('~'*len(startup_message))
     logger.info(startup_message)
     logger.info('~'*len(startup_message))
-    Settings.log()
+    Settings.Settings.log()
 
-    bot = DiscordBot()
+def reload_modules():
+    my_modules = [module for module in (set(sys.modules.values())-PRELOADED_MODULES) if module.__name__.startswith("src")]
+    for module in my_modules:
+        try:
+            importlib.reload(module)
+            print('reloaded: ', module)
 
-    run_state = True
-    if Settings.get('TEST_LOAD') == 1:
-        logger.info("Load was successful. Due to test load being enabled program will now exit.")
-        run_state = False
+        except :
+            print("Did not reload: ",module)
+    logger = Logger.Logger(__name__)
+    Settings.Settings.init()
 
-    while run_state:
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(await bot.run())
-    logger.info("Program Closed")
+async def main():
+    run_status = True
+    startup_message()
+    while run_status:
+        logger.info("Launching Bot")
+        bot = DiscordBot.DiscordBot(SQLITE_DB=Settings.Settings.get('SQLITE_DB'))
+        await bot.run()
+        run_status = bot.getRunStatus()
+        time.sleep(3)
+        if run_status:
+            logger.info("Bot is restarting")
+            reload_modules()
+        else:
+            logger.info("Bot is shutting down.")
+
+
 
 
 if __name__ == "__main__":
+    logger = Logger.Logger(__name__)
+    Settings.Settings.init()
     asyncio.run(main())
+
+
 
 
 
