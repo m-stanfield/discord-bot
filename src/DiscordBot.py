fa7cbedeb4369ea8e350b6e933569b4ad7b6b7b6
m-stanfield <mstanfield52@gmail.com>
2022-03-12 12:38:11 -0500

    removed run loop from Bot, now runs from main.py

--- src/DiscordBot.py
+++ src/DiscordBot.py
@@ -1,29 +1,23 @@
 import discord
-import asyncio
-
 from discord.ext.commands import Bot
-import time
 
-from importlib import reload
-from src.logging.logger import Logger
+import src.logging.logger as Logger
 from src.database.BaseDataBase import BaseDataBase
 from src.database.DiscordDataBase import DiscordDataBase
 from src.common.Settings import Settings
-
-
 import src.cogs.OwnerCommands as OwnerCommands
 
-cogList = [OwnerCommands]
+
+logger = Logger.Logger(__name__)
 
 
-
-logger = Logger(__name__)
-Settings.init()
 class DiscordBot(Bot):
 
-    DEFAULT_KWARGS = {'description':"default description","command_prefix":"!","intents":None}
+    DEFAULT_KWARGS = {'description': "default description",
+                      "command_prefix": "!", "intents": None}
+
     def __init__(self, **kwargs):
-        kwargs = {**self.DEFAULT_KWARGS,**kwargs}
+        kwargs = {**self.DEFAULT_KWARGS, **kwargs}
         if kwargs['intents'] is None:
             kwargs['intents'] = discord.Intents.all()
 
@@ -33,9 +27,10 @@ class DiscordBot(Bot):
             intents=kwargs.pop("intents")
         )
 
-        db:BaseDataBase = None if "db" not in kwargs else kwargs.pop('db')
-        if not(isinstance(db,BaseDataBase)):
-            path = Settings.get("SQLITE_DB") if "SQLITE_DB" not in kwargs else kwargs.pop("SQLITE_DB")
+        db: BaseDataBase = None if "db" not in kwargs else kwargs.pop('db')
+        if not(isinstance(db, BaseDataBase)):
+            path = Settings.get(
+                "SQLITE_DB") if "SQLITE_DB" not in kwargs else kwargs.pop("SQLITE_DB")
             db = DiscordDataBase(path=path)
         self.db = db
         self.add_cog(OwnerCommands.OwnerCommands(self))
@@ -45,7 +40,7 @@ class DiscordBot(Bot):
     async def close(self):
         await self.db.close()
         await super().close()
-    
+
     async def run(self):
         await self.db.init()
 
@@ -57,34 +52,10 @@ class DiscordBot(Bot):
             await self.db.close()
             await self.close()
 
-    async def setRunStatus(self, status:bool):
+    async def setRunStatus(self, status: bool):
         self.run_status = status
 
     def getRunStatus(self):
         return self.run_status
 
 
-
-
-        
-def reload_modules():
-    for module in cogList:
-        reload(module)
-
-
-
-if __name__ == "__main__":
-    async def main():
-        print(Settings.get('SQLITE_DB'))
-        run_status = True
-        while run_status:
-            reload_modules()
-            print(run_status)
-            bot = DiscordBot(SQLITE_DB=Settings.get('SQLITE_DB'))
-            await bot.run()
-            run_status = bot.getRunStatus()
-            time.sleep(10)
-            print('bot quit')
-            print(run_status)
-    asyncio.run(main())
-    
