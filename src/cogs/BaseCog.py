6ace34e9946fa51813334b6aba8bad0f7cb44b47
m-stanfield <mstanfield52@gmail.com>
2022-03-16 00:04:32 -0400

    added /play and ctx type hintint

--- src/cogs/BaseCog.py
+++ src/cogs/BaseCog.py
@@ -9,8 +9,7 @@ import src.common.Utilities as utils
 from discord.ext import commands
 from discord.ext.commands import Bot
 import time
-from discord.commands import slash_command
-from discord.ext.commands.context import Context
+from discord.commands import slash_command, ApplicationContext
 from src.logging.logger import Logger
 import inspirobot
 
@@ -26,7 +25,7 @@ class BaseCog(commands.Cog):
         self.bot: DiscordBot = bot
 
     @slash_command()
-    async def inspire(self, ctx, member: discord.Member | None = None):
+    async def inspire(self, ctx:ApplicationContext, member: discord.Member | None = None):
         output = ""
         if isinstance(member, discord.Member):
             output += utils.memberToMentionString(member=member)
@@ -35,13 +34,23 @@ class BaseCog(commands.Cog):
         await ctx.respond(output)
 
     @slash_command()
-    async def roll(self, ctx, roll_string=""):
+    async def roll(self, ctx:ApplicationContext, roll_string=""):
         '''Rolls dice. !roll <dice string>. Dice string exmaple: 1d20 -4d2 + 8 -2d2'''
         await ctx.respond(utils.roll_dice(roll_string))
 
     @slash_command()
-    async def nicknames(self, ctx, member: discord.Member | None = None, number: int = 5):
+    async def nicknames(self, ctx:ApplicationContext, member: discord.Member | None = None, number: int = 5):
         member = member if member is not None else ctx.author
         member = utils.memberToSchema(member)
         nickname_str = await self.bot.db.getMemberNicknames(member_values=member, number_of_nicknames=number)
         await ctx.respond(nickname_str)
+
+    @slash_command()
+    async def play(self, ctx:ApplicationContext, member: discord.Member | None = None, custom_audio:float=None):
+        channel = ctx.author.voice.channel
+        if channel is not None:
+            member = member if member is not None else ctx.author
+            user = utils.memberToSchema(member=member)
+            user = await self.bot.db.getValues(user=user)
+            await self.bot.playUserAudio(channel, member)
+
