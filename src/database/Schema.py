1e08d30e86dc6142b87191670e2d6c600a633626
m-stanfield <mstanfield52@gmail.com>
2022-05-16 22:27:03 -0400

    redoing schema and schema tests

--- src/database/Schema.py
+++ src/database/Schema.py
@@ -1,5 +1,5 @@
 from abc import abstractmethod, ABC
-from dataclasses import asdict, dataclass
+from dataclasses import asdict, dataclass, replace
 import dataclasses
 from dataclasses import InitVar
 from copy import deepcopy
@@ -8,8 +8,11 @@ from types import NoneType
 from typing import Any
 
 
+
 @dataclass
 class BaseSchema(ABC):
+    SEARCH_SET = set({})
+
     table_dict: InitVar[dict] = {}
     reset_values: InitVar[bool] = True
 
@@ -21,9 +24,9 @@ class BaseSchema(ABC):
                 if key in self.__dict__.keys():
                     self.__dict__[key] = value
 
-    @abstractmethod
     def toSearch(self):
-        pass
+        return self.get(self.SEARCH_SET) 
+
 
     def copy(self):
         return deepcopy(self)
@@ -33,18 +36,35 @@ class BaseSchema(ABC):
             if key in self.__dict__.keys():
                 self.__dict__[key] = None
 
-    def pop(self, key: str, default: Any = None):
+    def pop(self, key: str, default: Any = None)-> Any:
         popped_val = default
         if key in self.__dict__.keys():
             popped_val = self.__dict__[key]
             self.drop(key)
         return popped_val
 
+    def get(self, keys:list) -> dict[Any]:
+        result_dict = {}
+        allKeys = self.allKeys()
+        for key in keys:
+            if key in allKeys:
+                result_dict[key] = self.__dict__[key]
+        return result_dict
+
+    def set(self, **kwargs):
+        replacement_values = {}
+        # if a key exists in Schema, store in dictionary
+        allKeys = self.allKeys()
+        for key, val in kwargs.items():
+            if key in allKeys:
+                setattr(self, key,val)
+
+
     def reset(self):
         for key in self.__dict__:
             self.__dict__[key] = None
 
-    def toDict(self):
+    def toDict(self) -> dict[Any]:
         dictionary = asdict(self)
         output = {}
         for key, val in dictionary.items():
@@ -57,7 +77,7 @@ class BaseSchema(ABC):
             if key in self.allKeys():
                 self.__dict__[key] = value
 
-    def fromSchema(self, schema):
+    def fromSchema(self, schema:"BaseSchema"):
         self.fromDict(schema.toDict())
 
     @classmethod
@@ -65,62 +85,47 @@ class BaseSchema(ABC):
         name = self.__name__.replace("Schema", "").lower()
         return name
 
-    def allKeys(self):
-        return self.__dict__.keys()
+    def allKeys(self)->list[str]:
+        return list(self.__dict__.keys())
 
-    def allItems(self):
-        return self.__dict__.items()
+    def allItems(self)->list[str,Any]:
+        return list(self.__dict__.items())
 
-    def keys(self):
-        temp_dict = {key: value for key,
-                     value in self.__dict__.items() if value is not None}
-        return temp_dict.keys()
+    def keys(self) -> list[str]:
+        key_list = [key for key,
+                     value in self.__dict__.items() if value is not None]
+        return key_list
 
-    def items(self):
+    def items(self)-> list[Any]:
         temp_dict = {key: value for key,
                      value in self.__dict__.items() if value is not None}
         return temp_dict.items()
 
-    def __str__(self):
+    def __str__(self)-> str:
         output = ""
         for key in self.__dict__:
             output += f"{key}: {self.__dict__[key]}\n"
         return output
 
-    def __iter__(self):
-        yield from self.__dict__.items()
+    def __iter__(self) -> str:
+        yield from self.keys()
 
 
-def PyToSQLConverter(value):
-    val_type = type(value)
-    sql_type = "BLOB"
-    if val_type is float:
-        sql_type = "REAL"
-        value = float(value)
-    elif val_type is int or val_type is bool:
-        sql_type = "INTEGER"
-        value = int(value)
-    elif val_type is str:
-        sql_type = "TEXT"
-        value = str(value)
 
-    return sql_type, value
 
 
 @dataclass
 class GuildSchema(BaseSchema):
+    SEARCH_SET = {'guild_id'}
     guild_name: str = 'default_guild_name'
     guild_id: int = '-1'
     audio_enabled: int = '0'
 
-    def toSearch(self):
-        search_dict = {}
-        search_dict['guild_id'] = self.guild_id
-        return search_dict
-
 
 @dataclass
 class UserSchema(BaseSchema):
+    SEARCH_SET = {'guild_id','user_id'}
+
     guild_name: str = 'default_name'
     guild_id: int = -1
     user_id: int = -1
@@ -140,15 +145,11 @@ class UserSchema(BaseSchema):
     default_audio_path: str = None
     custom_audio_path: str = None
 
-    def toSearch(self):
-        search_dict = {}
-        search_dict['guild_id'] = self.guild_id
-        search_dict['user_id'] = self.user_id
-        return search_dict
 
 
 @dataclass
 class NicknamesSchema(BaseSchema):
+    SEARCH_SET = {'guild_id','user_id', 'timestamp'}
     guild_name: str = 'default_name'
     guild_id: int = -1
     user_id: int = -1
@@ -159,24 +160,29 @@ class NicknamesSchema(BaseSchema):
     def setTime(self, timestamp: float = -1):
         self.timestamp = timestamp if timestamp > 0 else time.time()
 
-    def toSearch(self):
-        search_dict = {}
-        search_dict['guild_id'] = self.guild_id
-        search_dict['user_id'] = self.user_id
-        search_dict['timestamp'] = self.timestamp
-        return search_dict
 
+def _PyToSQLConverter(value):
+    val_type = type(value)
+    sql_type = "BLOB"
+    if val_type is float:
+        sql_type = "REAL"
+        value = float(value)
+    elif val_type is int or val_type is bool:
+        sql_type = "INTEGER"
+        value = int(value)
+    elif val_type is str:
+        sql_type = "TEXT"
+        value = str(value)
+
+    return sql_type, value
 
 def _generateSchemaDict(var_dict: dict) -> dict:
     keys = var_dict.keys()
     value_dict = {}
     for key in keys:
-
         if dataclasses.is_dataclass(var_dict[key]) and key != BaseSchema.__name__:
-
-            value_dict[var_dict[key]().getTableName()] = {field.name: PyToSQLConverter(
+            value_dict[var_dict[key]().getTableName()] = {field.name: _PyToSQLConverter(
                 field.default) for field in dataclasses.fields(var_dict[key])}
-
     return value_dict
 
 
