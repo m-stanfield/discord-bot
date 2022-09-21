94599c043051e503ea91af88791b269291cf03f1
m-stanfield <mstan44@gmail.com>
2022-09-20 21:28:16 -0700

    Improving error messages and handling, fixing minor logic

--- src/cogs/EmoteCog.py
+++ src/cogs/EmoteCog.py
@@ -41,9 +41,9 @@ class EmoteCog(commands.Cog):
     async def autocomplete_emote(self, ctx: discord.AutocompleteContext):
         current_input = ctx.value.lower()
         results = await self.bot.db.getAllGuildEmoteEntries(guild=ctx.interaction.guild)
-        print(results)
+
         if (results):
-            return [result[0].emote_name for result in results if len(current_input) == 0 and result[0].emote_name.lower().startswith(current_input)] if type(results) == list else [results.emote_name]
+            return [result[0].emote_name for result in results if len(current_input) == 0 or result[0].emote_name.lower().startswith(current_input)] if type(results) == list else [results.emote_name]
         return []
 
     @slash_command(description="Post a custom server emote!")
@@ -67,7 +67,7 @@ class EmoteCog(commands.Cog):
    
         await ctx.delete()
         if not(ctx.channel.permissions_for(ctx.author).administrator):
-            ctx.author.send("Uploading an emote for a server requires being an admin for that server.")
+            await ctx.author.send("Uploading an emote for a server requires being an admin for that server.")
             return
 
         if not(ctx.guild):
